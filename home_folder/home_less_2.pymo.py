c2cd60ea2488a4c2c1b51abfe676d8662d0aab6c
vlad <vlad.moldovanov.2014@mail.ru>
2023-10-19 05:29:52 +0300

    create function transpose_matrix

--- home_folder/home_less_2.pymo.py
+++ home_folder/home_less_2.pymo.py
@@ -1,3 +1,7 @@
 import numpy
 # create random matrix
 matrix = numpy.random.random((3, 3))
+
+def transpose_matrix(matrix: list[list[int]]) -> list[list[int]]:
+    ''' This function transposes the matrix '''
+    return
